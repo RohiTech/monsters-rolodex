8b41796c75f3cde2fa333ecbe06875485a764a85
Jose Francisco <jfrancisco.fullstackdev@gmail.com>
2021-06-13 22:14:40 -0600

    Updates

---
+++ precache-manifest.2e85ed90ff92f4868e1abb4818f6371b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53b627d9886a40312ca01462a0a9ac6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ea2549ef15494ab23c99",
+    "url": "/monsters-rolodex/static/css/main.0cccd00d.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea2549ef15494ab23c99",
+    "url": "/monsters-rolodex/static/js/main.b88fe8b7.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
